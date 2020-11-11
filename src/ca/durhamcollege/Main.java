ffc17f262661e4d4f3a2d5c8f671c42904871426
ASHOK SASITHARAN <ASHOK2801@hotmail.com>
2020-11-11 10:40:35 -0500

    input and output test with two variables

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -13,10 +13,22 @@ public class Main
 
     public static void main(String[] args)
     {
-        String name = "";
+        //define scanner
         Scanner keyboard = new Scanner(System.in);
+
+        //variables
+        String name = "";
+        float age = 0.0f;
+        //Input
         System.out.print("Enter your name: ");
        name = keyboard.nextLine();
+
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+        //output
+        System.out.println();
+
         System.out.printf("You entered: %s",name);
+        System.out.printf("\nYou entered %.2f \n",age);
     }
 }
